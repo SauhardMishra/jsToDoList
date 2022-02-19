a8f880e3120a788e59632acd76b601fc232b7b39
SauhardMishra <sauhardmishra@cedcoss.com>
2022-02-19 16:51:25 +0530

    task commit

--- src/script.js
+++ src/script.js
@@ -10,8 +10,6 @@ function additem() {
   var todo = document.getElementById("new-task").value;
   //   console.log("In add");
 
-    
-
   tasks.push(todo);
   //    console.log(data);
   //  console.log(tasks);
@@ -20,13 +18,19 @@ function additem() {
 
 function display() {
   var ulData = document.getElementById("incomplete-tasks");
-  ulData.innerHTML="";
+  ulData.innerHTML = "";
   for (var i = 0; i < tasks.length; i++) {
     ulData.innerHTML +=
       '<li>\
-         <input type="checkbox" /><label>' +tasks[i] + '</label>\
-         <input type="text" /><button class="edit" onclick="editTask(\''+tasks[i]+'\')">Edit</button>\
-          <button  class="delete" onclick="removeTask(\'' +tasks[i] +"')\">Delete</button>\
+         <input type="checkbox" /><label>' +
+      tasks[i] +
+      '</label>\
+         <input type="text" /><button class="edit" onclick="editTask(\'' +
+      tasks[i] +
+      '\')">Edit</button>\
+          <button  class="delete" onclick="removeTask(\'' +
+      tasks[i] +
+      "')\">Delete</button>\
           </li>";
   }
   //   console.log(ulData);
@@ -37,21 +41,36 @@ function removeTask(task) {
   console.log("test okay");
   for (var i = 0; i < tasks.length; i++) {
     // var rem = tasks[i];
-    if (tasks[i]==task) {
-     tasks.splice(i,1);
+    if (tasks[i] == task) {
+      tasks.splice(i, 1);
     }
   }
   display();
 }
 
 function editTask(task) {
-  console.log("yes");
-  var p ={};
+  console.log(task);
+ 
   for (var i = 0; i < tasks.length; i++) {
-    if (tasks[i]==task) {
-      document.getElementById("new-task").value = tasks[i] ;
-     
+    if (tasks[i] == task) {
+      console.log(task);
+      document.getElementById("new-task").value = tasks[i];
+      document.getElementById("addbtn").style.display = "none";
+      document.getElementById("updatebtn").style.display = "block";
     }
   }
-  
+}
+
+function updateTask(task) {
+   console.log("update working");
+  var todo = document.getElementById("new-task").value;
+   for(var i=0;i<tasks.length;i++){
+     if(tasks[i]==todo){
+       console.log("test!!!!");
+          task[i] = todo; 
+     }
+   }
+   console.log(todo);
+   display(task);
+
 }
